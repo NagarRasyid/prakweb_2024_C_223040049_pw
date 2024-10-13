b3dc3185917e6303cf14ecb96c9dd1ca62cbb942
NagarRasyid <122325152+NagarRasyid@users.noreply.github.com>
2024-10-13 22:55:01 +0700

    Add files via upload

---
+++ OOP/constant.php
@@ -0,0 +1,38 @@
+<?php
+
+// define('NAMA', 'Nagar Rasyid');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+
+
+
+// class Coba {
+//     const NAMA = 'Nagar Rasyid';
+// }
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+?>
